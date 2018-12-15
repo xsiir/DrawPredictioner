e83b1e1c8c334d11db3587340a83c1c9ebe4b12c
Maciej <sienkiewicz.maciej971030@gmail.com>
2018-12-15 18:14:12 +0100

    Added java doco

--- src/pl/sienkiewicz/api/APIService.java
+++ src/pl/sienkiewicz/api/APIService.java
@@ -9,8 +9,29 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public interface APIService {
 	
 	
+	/**
+	 * 
+	 * @param leaugeCode kod ligi ktorej dane chcemy pobrac
+	 * @return odpowiedz zapisana w formacie JSON jako String
+	 * @throws UnirestException jesli nie uda sie poborac danych z API
+	 */
 	String callAPI(String leaugeCode) throws UnirestException;
+	
+	/**
+	 * 
+	 * 
+	 * @throws JsonSyntaxException jesli format JSONA bedzie wadliwy
+	 * @throws UnirestException jesli nie uda sie pobrac danych z API
+	 * @throws FileNotFoundException jesli nie znajdzie pliku .properties
+	 * @throws IOException jesli napotka blad z polaczeniem
+	 */
 	void getLeagueFixturesDetails() throws JsonSyntaxException, UnirestException, FileNotFoundException, IOException;
-	void printList(String type);
+	
+	/**
+	 * 
+	 * @param type rodzaj rozegranych meczy sluzacy do filtrowania wynikow - tu: TOTAL, AWAY, HOME
+	 */
+	void printList(String matchType);
+
 	
 }
